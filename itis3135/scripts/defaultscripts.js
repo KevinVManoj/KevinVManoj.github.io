9b1da2c003ac2907e701a2240fa5ac5192ac3e5c
Kevin Manoj <kmanoj@uncc.edu>
2024-02-15 02:05:57 -0500

    Activity 5 done

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -15,6 +15,37 @@ setInterval(fullDateTime, 1000);
 var userName = document.getElementById('userName');
 var userMood = document.getElementById('userMood');
 function userGreeting() {
-
     document.getElementById('siteResponse').innerHTML = `Welcome ${userName.value} to my website, you are currently feeling ${userMood.value}`;
 }
+
+function printAlphabetAlot() {
+    var numberTimes = document.getElementById('numberAlphabet').value;
+  
+    if (numberTimes !== "" && !isNaN(numberTimes)) {
+      var alphabetRepetitions = document.getElementById('alphabetRepitions');
+      alphabetRepetitions.innerText = "";
+  
+      for (let x = 0; x < Number(numberTimes); x++) {
+        alphabetRepetitions.innerText += "abcdefghijklmnopqrstuvwxyz \n";
+      }
+    } else {
+      alert("Please input a valid number");
+      return;
+    }
+  }
+
+
+  function randomYoutubeLink() {
+    var inputNumber = document.getElementById('randomYoutube').value;
+    var linkParagraph = document.getElementById('randomLink');
+
+    if (inputNumber === '1') {
+      linkParagraph.innerText = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    } else if (inputNumber === '2') {
+      linkParagraph.innerText = 'https://www.youtube.com/watch?v=jNQXAC9IVRw';
+    } else if (inputNumber === '3') {
+      linkParagraph.innerText = 'https://www.youtube.com/watch?v=AKJfakEsgy0';
+    } else {
+      linkParagraph.innerText = 'Invalid input. Please enter 1, 2, or 3.';
+    }
+  }
