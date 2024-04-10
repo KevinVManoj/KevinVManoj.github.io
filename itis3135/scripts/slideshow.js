cdf9cd0c82d94a26e5afb24ac4d272b382138046
Kevin Manoj <kmanoj@uncc.edu>
2024-04-10 01:22:55 -0400

    awqwe

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,30 +1,25 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
+  var i;
+  var slides = $(".mySlides"); 
+  var dots = $(".demo"); 
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  
+  slides.hide(); 
+  dots.removeClass("active");
+
+  slides.eq(slideIndex - 1).fadeIn(600); 
+  dots.eq(slideIndex - 1).addClass("active"); 
+  captionText.html(dots.eq(slideIndex - 1).attr("alt")); 
 }
